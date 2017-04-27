7068b0b2fea334bccd1f7e91bee3c9be9734547d
Nivedha <nivedha.v@mstsolutions.com>
2017-04-27 12:57:44 +0530

    Commit

---
+++ src/main/java/org/MavRelease/MavRelease/App.java
@@ -0,0 +1,13 @@
+package org.MavRelease.MavRelease;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
